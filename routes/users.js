0403cfdce37da32446e3b81a7e224a7b49fb10a6
NguyenDuong0609 <nguyenduong2824.com>
2021-10-05 20:15:43 +0700

    fix authenticate user

--- routes/users.js
+++ routes/users.js
@@ -5,30 +5,17 @@ const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserP
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
-// router.post('/register', registerUser);
-// router.post('/login', loginUser);
-// router.post('/password/forgot', forgotPassword);
-// router.put('/password/reset/:token', resetPassword);
-// router.get('/logout', logout);
-// router.get('/me', isAuthenticatedUser, getUserProfile);
-// router.put('/password/update', isAuthenticatedUser, updatePassword);
-// router.put('/me/update', isAuthenticatedUser, updateProfile);
-// router.get('/users/', isAuthenticatedUser, authorizeRoles('admin'), allUsers);
-// router.get('/user/:id', getUserDetails);
-// router.put('/user/:id', updateUser);
-// router.delete('/user/:id', authorizeRoles('admin'), deleteUser);
-
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/password/forgot', forgotPassword);
 router.put('/password/reset/:token', resetPassword);
 router.get('/logout', logout);
-router.get('/me', getUserProfile);
-router.put('/password/update', updatePassword);
-router.put('/me/update', updateProfile);
-router.get('/users/', allUsers);
+router.get('/me', isAuthenticatedUser, getUserProfile);
+router.put('/password/update', isAuthenticatedUser, updatePassword);
+router.put('/me/update', isAuthenticatedUser, updateProfile);
+router.get('/users/', isAuthenticatedUser, authorizeRoles('admin'), allUsers);
 router.get('/user/:id', getUserDetails);
 router.put('/user/:id', updateUser);
-router.delete('/user/:id', deleteUser);
+router.delete('/user/:id', authorizeRoles('admin'), deleteUser);
 
 module.exports = router;
